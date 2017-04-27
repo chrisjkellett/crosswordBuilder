dc7d6bbed5bc64600c330421c866edd579880eb7
chrisjkellett <cjkellett@hotmail.co.uk>
2017-04-27 15:14:45 +0200

    pre dead

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
-var getCrossword = document.querySelector('#crossword');
-var reduceRows = document.querySelector('#rowMinus');
-let cluebox = document.querySelector('.cluebox');
+const getCrossword = document.querySelector('#crossword');
+const reduceRows = document.querySelector('#rowMinus');
+const cluebox = document.querySelector('.cluebox');
+const answers = [];
+const maxSize = 10;
+const minSize = 4;
 let rowSize = 6;
 let gridinit = 1;
 let orientation;
 let counter = 0;
-let answers = [];
-const maxSize = 10;
-const minSize = 4;
+
 
 //generates grid
 function generateGrid(rowSize){
-    for (var i=1; i<rowSize + 1; i++){
+    for (let i=1; i<rowSize + 1; i++){
             getCrossword.innerHTML += `<div id="r-${i}" class="crossRow"></div>`;
         };
         makeCells(rowSize);
     };
 generateGrid(rowSize);
-var allCells = document.querySelectorAll('.crossBox');
+let allCells = document.querySelectorAll('.crossBox');
 
 
 function makeCells(rowSize){
-    for (var i=gridinit; i<rowSize + 1; i++){
-        var getRow = document.querySelector('#r-' + i);
+    for (let i=gridinit; i<rowSize + 1; i++){
+        let getRow = document.querySelector('#r-' + i);
         gridinit = rowSize + 1;
-        for (var j=1; j<rowSize + 1; j++){
+        for (let j=1; j<rowSize + 1; j++){
             getRow.innerHTML += `<div class="cell-wrapper">
             <input type="text" maxlength="1" id="${i}.${j}" class="crossBox crossFont" /></div>`;
             };
@@ -32,8 +33,8 @@ function makeCells(rowSize){
 }
 
 function addToColumns(rowSize){
-    for (var j=1; j<rowSize; j++){
-        var getRow = document.querySelector('#r-' + j);
+    for (let j=1; j<rowSize; j++){
+        let getRow = document.querySelector('#r-' + j);
             getRow.insertAdjacentHTML('beforeend', 
             `<div class="cell-wrapper">
             <input type="text" maxlength="1" id="${j}.${rowSize}" class="crossBox crossFont" /></div>`);
@@ -42,28 +43,28 @@ function addToColumns(rowSize){
 
 
 //increase grid rowSize
-var addRows = document.querySelector('#rowPlus');
+const addRows = document.querySelector('#rowPlus');
 addRows.addEventListener('click', function(){
     if (rowSize < maxSize){
         rowSize += 1;
-        var addNewRow = getCrossword.insertAdjacentHTML('beforeend', `<div id="r-${rowSize}" class="crossRow"></div>`);
+        let addNewRow = getCrossword.insertAdjacentHTML('beforeend', `<div id="r-${rowSize}" class="crossRow"></div>`);
         makeCells(rowSize);
         addToColumns(rowSize);
         allCells = document.querySelectorAll('.crossBox');
-        for (var i=0; i<initWordId.length; i++){
+        for (let i=0; i<initWordId.length; i++){
             validateCrossword(initWordId[i]);
         };
     };
 });
 
 //decrease grid size
-var minusRows = document.querySelector('#rowMinus');
+const minusRows = document.querySelector('#rowMinus');
 minusRows.addEventListener('click', function(){
     if (rowSize > minSize){
-        var row = document.querySelector('#r-' + (rowSize - 1));
+        let row = document.querySelector('#r-' + (rowSize - 1));
         row.nextElementSibling.remove();
-        for (var j=1; j<rowSize; j++){
-            var row = document.querySelector('#r-' + j);
+        for (let j=1; j<rowSize; j++){
+            let row = document.querySelector('#r-' + j);
             row.lastElementChild.remove();
         };
         allCells = document.querySelectorAll('.crossBox');
@@ -86,11 +87,11 @@ if(initWordId.length <= 1){
 }
 
 //adds listener to children of getCrossword
-var initWordId = [];
+let initWordId = [];
 getCrossword.addEventListener('keyup', getBox, false);
 function getBox(el) {
     if (el.target !== el.currentTarget) {
-        var clickedItem = el.target;
+        let clickedItem = el.target;
         if (!clickedItem.className.includes('selected')){
             clickedItem.className += ' selected';
         };
@@ -98,14 +99,14 @@ function getBox(el) {
             clickedItem.classList.remove('selected'); 
         };
         id = el.target.id;
-        if (!initWordId.includes(id) && el.target.className.includes('selected')){7
+        if (!initWordId.includes(id) && el.target.className.includes('selected')){
         initWordId.push(id);
         validateCrossword(id);
         };
    };
     el.stopPropagation();
     for (let id of initWordId){
-        var el = document.getElementById(id);
+        let el = document.getElementById(id);
         if (!el.className.includes('selected')){
             initWordId.pop(id);
             };
@@ -114,6 +115,7 @@ function getBox(el) {
     word_length(initWordId);
     check_gaps(initWordId);
     validateLoop(initWordId);
+    //validatedeadCells
 }
 
 getCrossword.addEventListener('click', getSavedBox, false);
@@ -133,47 +135,21 @@ function getSavedBox(el) {
 
 
 function validateCrossword(id){
-    var selectedIdRef = id.split(".");
-    var col = selectedIdRef[0];
-    var row = selectedIdRef[1];
+    let deadCells = document.querySelectorAll('.deadCell');
+    let selectedIdRef = id.split(".");
+    let col = selectedIdRef[0];
+    let row = selectedIdRef[1];
     for(cell of allCells){
-        var loopIdRef = cell.id.split(".");
-        var loopCol = loopIdRef[0];
-        var loopRow = loopIdRef[1];
+        let loopIdRef = cell.id.split(".");
+        let loopCol = loopIdRef[0];
+        let loopRow = loopIdRef[1];
         if (!(row == loopRow || col == loopCol)){
             cell.disabled = true;
         };
     };
 }
 
-
-
-//4a. add word button listener
-var addWordBtn = document.querySelector('#addWord');
-addWordBtn.addEventListener('click', function(){
-    let getLetters = document.querySelectorAll('.selected');
-    let clue = '';
-    for (let cell of allCells){
-            cell.disabled = true;
-        };
-    for (let letter of getLetters){
-        clue += letter.value.toLowerCase();
-        letter.style.background = 'white';  
-    };
-    addWordBtn.disabled = true;
-    let insertClue = document.querySelector('#insertClue');
-    insertClue.textContent = clue;
-    if (!document.getElementById(initWordId[0]).previousElementSibling){
-        counter += 1;
-    };
-    let insertLocation = document.querySelector('#insertLocation');
-    insertLocation.textContent = counter + " " + orientation;
-    cluebox.style.display = 'block';
-    answers.push(clue);
-});
-
-
-//4bi. check word length and return false if too short
+//F1. check word length and return false if too short
 function word_length(ids){
     if (ids.length < 2){
         addWordBtn.disabled = true;
@@ -183,7 +159,7 @@ function word_length(ids){
 };
 
 
-//4bii. make sure there are no gaps in words and return false if there is
+//F2. make sure there are no gaps in words and return false if there is
 function check_gaps(ids){
     let col_list = [];
     let row_list = [];
@@ -215,11 +191,63 @@ function check_gaps(ids){
 
 }
 
+//F.reset grid
+function resetGrid(){
+    //validateWithClues(initWordId);
+    initWordId = [];
+    let getCells = document.querySelectorAll('.crossBox');
+        for (cell of getCells){
+            cell.disabled = false;
+        };
+}
 
-//5a. confirm clue and add to clueList
+
+
+//3. add/initialise clue
+const addWordBtn = document.querySelector('#addWord');
+addWordBtn.addEventListener('click', function(){
+    //i get all cells with clue
+    let getLetters = document.querySelectorAll('.selected');
+
+    //ii. init clue variable
+    let clue = '';
+
+    //iii.disable everything
+    for (let cell of allCells){
+            cell.disabled = true;
+        };
+    addWordBtn.disabled = true;
+
+    //iv.change style for clue cells
+    for (let letter of getLetters){
+        clue += letter.value.toLowerCase();
+        letter.style.background = 'white';  
+    };
+
+    //v. increase counter if required
+    if (!document.getElementById(initWordId[0]).previousElementSibling){
+        counter += 1;
+    };
+
+    //vi. insert clue into clueBox
+    let insertClue = document.querySelector('#insertClue');
+    insertClue.textContent = clue;
+    let insertLocation = document.querySelector('#insertLocation');
+    insertLocation.textContent = counter + " " + orientation;
+
+    //vii. show clueBox
+    cluebox.style.display = 'block';
+
+    //viii.push clue to answer list
+    answers.push(clue);
+});
+
+
+
+//4. confirm clue and add to clueList
 let confirmClueBtn = document.querySelector('#confirmClue');
 confirmClueBtn.addEventListener('click', function(){
-    //5ai. adds and removes classe  
+    //i. adds and removes classes  
     let initLetterId = document.getElementById(initWordId[0]);
     for (let letter of initWordId){
         let getCell = document.getElementById(letter);
@@ -227,11 +255,11 @@ confirmClueBtn.addEventListener('click', function(){
         getCell.classList.remove('selected');
         getCell.classList.remove('crossBox');
     };
-    //5aii. adds number to firstLetter
+    //ii. adds number to firstLetter
     initLetterId.insertAdjacentHTML('beforeBegin', 
             `<div class="number-wrapper">${counter}</div>`);
 
-    //5aiii. adds clues to clueList
+    //iii. adds clues to clueList
     let getClueList = document.getElementById(`${orientation}`);
     const getInput = document.getElementById('clueEntry');
     let getInputVal = getInput.value;
@@ -247,20 +275,10 @@ confirmClueBtn.addEventListener('click', function(){
     console.log(el);
     getClueList.appendChild(el);
 
-    //5aiv. resets grid for next clue
+    //iv. resets grid for next clue
     resetGrid();
 });
 
-
-//5b.reset grid
-function resetGrid(){
-    //validateWithClues(initWordId);
-    initWordId = [];
-    let getCells = document.querySelectorAll('.crossBox');
-        for (cell of getCells){
-            cell.disabled = false;
-        };
-}
 
 //5b. cancel clue
 let cancelClueBtn = document.querySelector('#cancelClue');
