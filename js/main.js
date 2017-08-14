196f963e3f2e15805138a8c3d88df6ff1b76b27f
Christopher <cjkellett@hotmail.co.uk>
2017-08-14 15:37:22 +0200

    jquerified

--- js/main.js
+++ js/main.js
@@ -1,66 +1,99 @@
-!function(){
-  const crossword = {
 
-    settings: {
-      rows: 6,
-      gridInitValue: 1,
-      max: 10
-    },
+const crossword = {
+  
+  settings: {
+    rows: 6,
+    gridInitValue: 1,
+    max: 10,
+    min: 4,
+  },
 
-    init: function(){
-      this.generateGrid(this.settings.rows);
-      this.increaseGridBtn(this.settings.rows, this.settings.max);
-      this.cacheDOM();
-    },
+  values: {
+    currentWord: [],
+    allCells: []
+  },
 
-    cacheDOM: function(){
-      this.$wrapper = $('div#crosswordWrapper');
-      console.log(this.$wrapper);
-    },
-    
-    generateGrid: function(rows){
-      const $crossword = document.getElementById('crossword');
-      for (let i = 1; i < rows + 1; i++)
-        $crossword.innerHTML += `<div id="r-${i}" class="_row"></div>`;
-      this.makeCells(this.settings.rows, this.settings.gridInitValue);
-    },
+  init: function(){
+    this.generateGrid(this.settings.rows, this.DOM.crossword);
+    this.increaseGridBtn(this.settings.rows, this.settings.max, this.DOM.increaseBtn, this.values.allCells);
+    this.selectCells(this.values.currentWord, this.DOM.crossword);
+  },
 
-    makeCells: function(rows, init){
-      for (let i = init; i < rows + 1; i++){
-        let row = document.querySelector('#r-' + i);
-        init = rows + 1;
-        for (let j = 1; j < rows + 1; j++){
-          row.innerHTML += `<div class="cell-wrapper">
-          <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`;
-        }//end for
-      }//end for
-      this.settings.gridInitValue = init;
-    },
+  DOM: {
+    crossword: document.getElementById('crossword'),
+    increaseBtn: document.getElementById('increaseBtn')
+  },
+  
+  generateGrid: function(rows, $crossword){
+    for (let i = 1; i < rows + 1; i++)
+      $crossword.innerHTML += `<div id="r-${i}" class="_row"></div>`;
+    this.makeCells(this.settings.rows, this.settings.gridInitValue);
+    this.cacheCells(this.values.allCells);
+  },
 
-    increaseGridBtn: function(rows, max){
-      const increaseBtn = document.getElementById('increaseBtn');
-      increaseBtn.addEventListener('click', function(){
-        if (rows < max){
-          rows ++;
-          const $crossword = document.getElementById('crossword');
-          const addNewRow = $crossword.insertAdjacentHTML('beforeend', 
-          `<div id="r-${rows}" class="_row"></div>`);
-          for (let i = 1; i < rows; i++){
-            let row = document.querySelector('#r-' + i);
-            row.insertAdjacentHTML('beforeend', 
-            `<div class="cell-wrapper">
-            <input type="text" maxlength="1" id="${i}.${rows}" class="cell row-${i} col-${rows}" /></div>`);
-          }//end for
-          crossword.makeCells(rows, crossword.settings.gridInitValue);
-        }else{
-          console.log('Exceeds grid size');
-        }//end if 
-      });//end listener
+  makeCells: function(rows, init){
+    for (let i = init; i < rows + 1; i++){
+      let row = document.querySelector('#r-' + i);
+      init = rows + 1;
+      for (let j = 1; j < rows + 1; j++){
+        row.innerHTML += `<div class="cell-wrapper">
+        <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`;
+      }
     }
+    this.settings.gridInitValue = init;
+  },
 
-  }//end object
-  crossword.init();
-}();
+  cacheCells: function(allCells){
+    allCells = document.querySelectorAll('.cell');
+    console.log(allCells);
+  },
+
+  increaseGridBtn: function(rows, max, $button, allCells){
+    $button.addEventListener('click', function(){
+      if (rows < max){
+        rows ++;
+        const $crossword = document.getElementById('crossword');
+        const addNewRow = $crossword.insertAdjacentHTML('beforeend', `<div id="r-${rows}" class="_row"></div>`);
+        for (let i = 1; i < rows; i++){
+          let row = document.querySelector('#r-' + i);
+          row.insertAdjacentHTML('beforeend', 
+          `<div class="cell-wrapper">
+          <input type="text" maxlength="1" id="${i}.${rows}" class="cell row-${i} col-${rows}" /></div>`);
+        }//end for
+        crossword.makeCells(rows, crossword.settings.gridInitValue);
+      }else{
+        console.log('Exceeds grid size');
+      }//end if 
+      crossword.cacheCells(allCells);
+    });//end listener
+  },
+
+  selectCells: function(currentWord, $crossword){
+    $crossword.addEventListener('keyup', function(e){
+      const cell = e.target;
+      const id = cell.id;
+      const isSelected = cell.className.includes('selected');
+      const notCurrentWord = !currentWord.includes(id);
+      const hasValue = cell.value === '';
+
+      if (!isSelected && !hasValue && notCurrentWord){
+        cell.classList.add('selected');
+        currentWord.push(id);
+        crossword.validateGrid(crossword.DOM.crossword);
+      } else if (isSelected && hasValue){
+        cell.classList.remove('selected');
+        currentWord.pop(id);
+        // sortAndValidate(id);
+      }
+    });
+  },
+
+  validateGrid: function($allCells){
+    console.log($allCells);
+  }
+
+}//end object
+crossword.init();
 
 
 
--- js/test.js
+++ js/test.js
@@ -1,60 +1,92 @@
-!function(){
-  const crossword = {
 
-    settings: {
-      rows: 6,
-      gridInitValue: 1,
-      max: 10
-    },
+const module = {
+  
+  settings: {
+    rows: 6,
+    init: 1,
+    max: 10,
+    min: 4,
+  },
 
-    init: function(){
-      this.generateGrid(this.settings.rows);
-      this.increaseGridBtn(this.settings.rows, this.settings.max);
-    },
-    
-    generateGrid: function(rows){
-      const $crossword = document.getElementById('crossword');
-      for (let i = 1; i < rows + 1; i++)
-        $crossword.innerHTML += `<div id="r-${i}" class="_row"></div>`;
-      this.makeCells(this.settings.rows, this.settings.gridInitValue);
-    },
+  values: {
+    currentWord: [],
+  },
 
-    makeCells: function(rows, init){
-      for (let i = init; i < rows + 1; i++){
-        let row = document.querySelector('#r-' + i);
-        init = rows + 1;
-        for (let j = 1; j < rows + 1; j++){
-          row.innerHTML += `<div class="cell-wrapper">
-          <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`;
-        }//end for
-      }//end for
-      this.settings.gridInitValue = init;
-    },
 
-    increaseGridBtn: function(rows, max){
-      const increaseBtn = document.getElementById('increaseBtn');
-      increaseBtn.addEventListener('click', function(){
-        if (rows < max){
-          rows ++;
-          const $crossword = document.getElementById('crossword');
-          const addNewRow = $crossword.insertAdjacentHTML('beforeend', 
-          `<div id="r-${rows}" class="_row"></div>`);
-          for (let i = 1; i < rows; i++){
-            let row = document.querySelector('#r-' + i);
-            row.insertAdjacentHTML('beforeend', 
-            `<div class="cell-wrapper">
-            <input type="text" maxlength="1" id="${i}.${rows}" class="cell row-${i} col-${rows}" /></div>`);
-          }//end for
-          crossword.makeCells(rows, crossword.settings.gridInitValue);
-        }else{
-          console.log('Exceeds grid size');
-        }//end if 
-      });//end listener
-    }//end function
+  init: function(){
+    this.cacheDOM();
+    this.generateGrid(this.settings.rows);
+    this.increaseGridBtn(this.settings.rows, this.settings.max);
+    this.selectCells(this.values.currentWord);
+  },
 
-  }//end object
-  crossword.init();
-}();
+
+  cacheDOM: function(){
+    this.$wrapper = $('#crosswordWrapper');
+    this.$crossword = this.$wrapper.find('#crossword');
+    this.$increaseBtn = this.$wrapper.find('#increaseBtn');
+    this.$allCells = this.$wrapper.find('.cell');
+  },
+  
+  
+  generateGrid: function(rows){
+    for (let i = 1; i < rows + 1; i++){
+      let newItem = $(`<div id="r-${i}" class="_row"></div>`);
+      this.$crossword.append(newItem);
+    }
+    this.makeCells(this.settings.rows, this.settings.init);
+  },
+
+  makeCells: function(rows, init){
+    for (let i = init; i < rows + 1; i++){
+      let $row = this.$wrapper.find('#r-' + i);
+      init = rows + 1;
+      for (let j = 1; j < rows + 1; j++){
+        let newItem = $(`<div class="cell-wrapper">\
+        <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`);
+        $row.append(newItem);
+      };
+    };
+    this.settings.init = init;
+    this.cacheCells();
+  },
+
+  cacheCells: function(){
+    this.$allCells = this.$wrapper.find('.cell');
+    //console.log(this.$allCells);
+  },
+
+  increaseGridBtn: function(rows, max){
+    this.$increaseBtn.click(function(){
+      if (rows < max){
+        rows ++;
+        let newItem = $(`<div id="r-${rows}" class="_row"></div>`);
+        module.$crossword.append(newItem);
+        for (let i = 1; i < rows; i++){
+          let $row = module.$wrapper.find('#r-' + i);
+          let newItem = $(`<div class="cell-wrapper">\
+          <input type="text" maxlength="1" id="${i}.${rows}" class="cell row-${i} col-${rows}" /></div>`);
+          $row.append(newItem);
+        }
+        module.makeCells(rows, module.settings.init);
+      }else{
+        console.log('Exceeds grid size');
+      }
+    });//end listener
+  },
+
+  selectCells: function(currentWord){
+    this.$crossword.keyup(function(e){
+      console.log(e.target);
+    });
+  },
+
+  validateGrid: function($allCells){
+    console.log($allCells);
+  }
+
+}//end object
+module.init();
 
 
 
