6ccfa128b762838753e56ec0f9d8841b73484b1f
Christopher <cjkellett@hotmail.co.uk>
2017-08-15 20:39:35 +0200

    added spacing and removed dead refs

--- js/test.js
+++ js/test.js
@@ -8,16 +8,17 @@
       this.min = 4;
       this.currentWord = [];
       this.orientation = 'across';
-      this.validateCount = 0;
       this.myKeyCommands = false;
     },
 
+    
     init: function(){
       this.settings();
       this.cacheDOM();
       this.bindEvents();
       this.generateGrid();
     },
+
 
     cacheDOM: function(){
       this.$wrapper = $('#crosswordWrapper');
@@ -34,6 +35,7 @@
       this.$addWordBtn = this.$wrapper.find('#addWordBtn');
     },
 
+
     bindEvents: function(){
       this.$crossword.keyup(this.validateCells.bind(this));
       this.$crossword.keyup(this.navigateGrid.bind(this));
@@ -44,47 +46,51 @@
       this.$addWordBtn.click(this.renderClue.bind(this));
     },
 
+
     runCrosswordEvents: function(e){
       this.validateCells(e);
       this.navigateGrid(e);
     },
     
+
     generateGrid: function(){
       for (let i = 1; i < this.rows + 1; i++){
         let newItem = $(`<div id="r-${i}" class="_row"></div>`);
         this.$crossword.append(newItem);
       }
-      
       this.makeCells();
     },
+
 
     makeCells: function(){
       for (let i = this.init; i < this.rows + 1; i++){
         let $row = this.$wrapper.find('#r-' + i);
         this.init = this.rows + 1;
-
         for (let j = 1; j < this.rows + 1; j++){
           let newItem = $(`<div class="cell-wrapper">\
           <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`);
           $row.append(newItem);
         }
-
       }
       this.cacheCells();
     },
 
+
     cacheCells: function(){
       this.$allCells = this.$wrapper.find('.cell');
     },
+
 
     alertBox: function(message){
       this.$alertMessage.text(message);
       this.$alertBox.css('display', 'block');
     },
 
+
     alertBoxConfirm: function(){
       this.$alertBox.css('display', 'none');
     },
+
 
     increaseSize: function(){
       if (this.rows < this.max){
@@ -110,42 +116,34 @@
       }
     },
 
+
     decreaseSize: function(){
       const message = 'Not currently working';
       this.alertBox(message);
     },
 
+
     navigateGrid: function(e){
       if (e.keyCode === 37){
-        this.myKeyCommands = true;
         const i = this.$allCells.index(e.target);
         const item = this.$allCells.get(i - 1);
         item.focus();
       }
-
       else if (e.keyCode === 38){
-        this.myKeyCommands = true;
         const i = this.$allCells.index(e.target);
         const item = this.$allCells.get(i - this.rows);
         item.focus();
       }
-
       else if (e.keyCode === 39){
-        this.myKeyCommands = true;
         const i = this.$allCells.index(e.target);
         const item = this.$allCells.get(i + 1);
         if (item ? item.focus() : console.log('cannot go further'));
       }
-
       else if (e.keyCode === 40){
-        this.myKeyCommands = true;
         const i = this.$allCells.index(e.target);
         const item = this.$allCells.get(i + this.rows);
         if (item ? item.focus() : console.log('cannot go further'));
       }
-      else
-        this.myKeyCommands = false;
-
     },
 
     validateCells: function(e){
@@ -159,20 +157,22 @@
         cell.classList.add('selected');
         this.currentWord.push(id);
         this.validateGrid(id);
+        this.validateWordLength();
+        this.validateWordStructure();
+        this.validateReset();
       }
-
       else if (a && b){
         cell.classList.remove('selected');
         const i = this.currentWord.indexOf(id);
         this.currentWord.splice(i, 1);
         this.validateGrid(id);
+        this.validateWordLength();
+        this.validateWordStructure();
+        this.validateReset();
       }
-
-      this.validateWordLength();
-      this.validateWordStructure();
-      this.validateReset();
       //console.log(this.currentWord);
     },
+
 
     validateGrid: function(id){
       const sp = id.split(".");
@@ -188,6 +188,7 @@
       }
     },
 
+
     validateWordLength: function(){
       if (this.currentWord.length < 2)
         this.$addWordBtn.attr('disabled', true);
@@ -195,18 +196,17 @@
         this.$addWordBtn.removeAttr('disabled');
     },
 
+
     validateWordStructure: function(){
       this.currentWord.sort();
       let cols = []; 
       let rows = [];
       let testFails, isRow, isColumn;
-
       for (let id of this.currentWord){
         let sp = id.split(".");
         cols.push(parseInt(sp[0]));
         rows.push(parseInt(sp[1]));
       }
-
       for (let i = cols.length - 1; i > 0; i--){
         let j = i - 1;
         if (cols[i] - cols[j] > 1)
@@ -218,7 +218,6 @@
         else if (rows[i] - rows[j] === 0)
           isColumn = true;
       }
-
       if(testFails)
         this.$addWordBtn.attr('disabled', true);
       else if(isRow){
@@ -229,6 +228,7 @@
       }
     },
 
+
     validateInput: function(e){
       if (e.charCode < 64 || e.charCode > 122) {
         const message = 'Non alphanumeric characters cannot form part of a word';
@@ -237,6 +237,7 @@
       }
     },
 
+
     validateReset: function(){
       if(this.currentWord.length < 2){
         for (let cell of this.$allCells){
@@ -244,18 +245,16 @@
           //##this area needs work to avoid unnecessary 2 step resets
         }
       }
-       
       if (this.currentWord.length === 1)
         this.validateGrid(this.currentWord[0]);
-
-      if (this.currentWord.length === 0)
-        this.validateCount = 0;
     },
+
 
     renderClue: function(){
       this.addClueAndReference();
       this.showPromptBox();
     },
+
 
     captureClue: function(){
       for (let id of this.currentWord){
@@ -264,15 +263,18 @@
       }
     },
 
+
     addClueAndReference: function(){
       this.$insertClue = this.captureClue();
     },
+
 
     showPromptBox: function(){
       this.$clueBox.css('display', 'block');
     }
 
   }//end object
+
   module.init();
 })();
 
