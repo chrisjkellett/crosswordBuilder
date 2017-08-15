4550e4854bb933c4b8e89cc6fe76da97cde27042
Christopher <cjkellett@hotmail.co.uk>
2017-08-15 13:46:56 +0200

    keystrokes

--- js/main.js
+++ js/main.js
@@ -1,90 +1,83 @@
 
-const crossword = {
+const module = {
   
   settings: {
     rows: 6,
-    gridInitValue: 1,
+    init: 1,
     max: 10,
     min: 4,
   },
 
   values: {
     currentWord: [],
-    allCells: []
   },
+
 
   init: function(){
-    this.generateGrid(this.settings.rows, this.DOM.crossword);
-    this.increaseGridBtn(this.settings.rows, this.settings.max, this.DOM.increaseBtn, this.values.allCells);
-    this.selectCells(this.values.currentWord, this.DOM.crossword);
+    this.cacheDOM();
+    this.generateGrid(this.settings.rows);
+    this.increaseGridBtn(this.settings.rows, this.settings.max);
+    this.selectCells(this.values.currentWord);
   },
 
-  DOM: {
-    crossword: document.getElementById('crossword'),
-    increaseBtn: document.getElementById('increaseBtn')
+
+  cacheDOM: function(){
+    this.$wrapper = $('#crosswordWrapper');
+    this.$crossword = this.$wrapper.find('#crossword');
+    this.$increaseBtn = this.$wrapper.find('#increaseBtn');
+    this.$allCells = this.$wrapper.find('.cell');
   },
   
-  generateGrid: function(rows, $crossword){
-    for (let i = 1; i < rows + 1; i++)
-      $crossword.innerHTML += `<div id="r-${i}" class="_row"></div>`;
-    this.makeCells(this.settings.rows, this.settings.gridInitValue);
-    this.cacheCells(this.values.allCells);
+  
+  generateGrid: function(rows){
+    for (let i = 1; i < rows + 1; i++){
+      let newItem = $(`<div id="r-${i}" class="_row"></div>`);
+      this.$crossword.append(newItem);
+    }
+    this.makeCells(this.settings.rows, this.settings.init);
   },
 
   makeCells: function(rows, init){
     for (let i = init; i < rows + 1; i++){
-      let row = document.querySelector('#r-' + i);
+      let $row = this.$wrapper.find('#r-' + i);
       init = rows + 1;
       for (let j = 1; j < rows + 1; j++){
-        row.innerHTML += `<div class="cell-wrapper">
-        <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`;
-      }
-    }
-    this.settings.gridInitValue = init;
+        let newItem = $(`<div class="cell-wrapper">\
+        <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`);
+        $row.append(newItem);
+      };
+    };
+    this.settings.init = init;
+    this.cacheCells();
   },
 
-  cacheCells: function(allCells){
-    allCells = document.querySelectorAll('.cell');
-    console.log(allCells);
+  cacheCells: function(){
+    this.$allCells = this.$wrapper.find('.cell');
+    //console.log(this.$allCells);
   },
 
-  increaseGridBtn: function(rows, max, $button, allCells){
-    $button.addEventListener('click', function(){
+  increaseGridBtn: function(rows, max){
+    this.$increaseBtn.click(function(){
       if (rows < max){
         rows ++;
-        const $crossword = document.getElementById('crossword');
-        const addNewRow = $crossword.insertAdjacentHTML('beforeend', `<div id="r-${rows}" class="_row"></div>`);
+        let newItem = $(`<div id="r-${rows}" class="_row"></div>`);
+        module.$crossword.append(newItem);
         for (let i = 1; i < rows; i++){
-          let row = document.querySelector('#r-' + i);
-          row.insertAdjacentHTML('beforeend', 
-          `<div class="cell-wrapper">
+          let $row = module.$wrapper.find('#r-' + i);
+          let newItem = $(`<div class="cell-wrapper">\
           <input type="text" maxlength="1" id="${i}.${rows}" class="cell row-${i} col-${rows}" /></div>`);
-        }//end for
-        crossword.makeCells(rows, crossword.settings.gridInitValue);
+          $row.append(newItem);
+        }
+        module.makeCells(rows, module.settings.init);
       }else{
         console.log('Exceeds grid size');
-      }//end if 
-      crossword.cacheCells(allCells);
+      }
     });//end listener
   },
 
-  selectCells: function(currentWord, $crossword){
-    $crossword.addEventListener('keyup', function(e){
-      const cell = e.target;
-      const id = cell.id;
-      const isSelected = cell.className.includes('selected');
-      const notCurrentWord = !currentWord.includes(id);
-      const hasValue = cell.value === '';
-
-      if (!isSelected && !hasValue && notCurrentWord){
-        cell.classList.add('selected');
-        currentWord.push(id);
-        crossword.validateGrid(crossword.DOM.crossword);
-      } else if (isSelected && hasValue){
-        cell.classList.remove('selected');
-        currentWord.pop(id);
-        // sortAndValidate(id);
-      }
+  selectCells: function(currentWord){
+    this.$crossword.keyup(function(e){
+      console.log(e.target);
     });
   },
 
@@ -93,7 +86,7 @@ const crossword = {
   }
 
 }//end object
-crossword.init();
+module.init();
 
 
 
--- js/test.js
+++ js/test.js
@@ -1,93 +1,208 @@
 
-const module = {
-  
-  settings: {
-    rows: 6,
-    init: 1,
-    max: 10,
-    min: 4,
-  },
+(function(){
+  const module = {
+    settings: function(){
+      this.rows = 6;
+      this.init = 1;
+      this.max = 10;
+      this.min = 4;
+      this.currentWord = [];
+      this.orientation = 'across';
+    },
 
-  values: {
-    currentWord: [],
-  },
+    init: function(){
+      this.settings();
+      this.cacheDOM();
+      this.bindEvents();
+      this.generateGrid();
+    },
 
+    cacheDOM: function(){
+      this.$wrapper = $('#crosswordWrapper');
+      this.$crossword = this.$wrapper.find('#crossword');
+      this.$increaseBtn = this.$wrapper.find('#increaseBtn');
+      this.$allCells = this.$wrapper.find('.cell');
+      this.$alertBox = this.$wrapper.find('#alertBox');
+      this.$alertMessage = this.$wrapper.find('#alertMessage');
+      this.$alertConfirm = this.$wrapper.find('#okAlert');
+      this.$addWordBtn = this.$wrapper.find('#addWordBtn');
+    },
 
-  init: function(){
-    this.cacheDOM();
-    this.generateGrid(this.settings.rows);
-    this.increaseGridBtn(this.settings.rows, this.settings.max);
-    this.selectCells(this.values.currentWord);
-  },
+    bindEvents: function(){
+      this.$crossword.keyup(this.runCrosswordEvents.bind(this));
+      this.$increaseBtn.click(this.increaseSize.bind(this));
+      this.$alertConfirm.click(this.alertBoxConfirm.bind(this));
+    },
+    
+    generateGrid: function(){
+      for (let i = 1; i < this.rows + 1; i++){
+        let newItem = $(`<div id="r-${i}" class="_row"></div>`);
+        this.$crossword.append(newItem);
+      }
+      
+      this.makeCells();
+    },
 
+    makeCells: function(){
+      for (let i = this.init; i < this.rows + 1; i++){
+        let $row = this.$wrapper.find('#r-' + i);
+        this.init = this.rows + 1;
 
-  cacheDOM: function(){
-    this.$wrapper = $('#crosswordWrapper');
-    this.$crossword = this.$wrapper.find('#crossword');
-    this.$increaseBtn = this.$wrapper.find('#increaseBtn');
-    this.$allCells = this.$wrapper.find('.cell');
-  },
-  
-  
-  generateGrid: function(rows){
-    for (let i = 1; i < rows + 1; i++){
-      let newItem = $(`<div id="r-${i}" class="_row"></div>`);
-      this.$crossword.append(newItem);
-    }
-    this.makeCells(this.settings.rows, this.settings.init);
-  },
-
-  makeCells: function(rows, init){
-    for (let i = init; i < rows + 1; i++){
-      let $row = this.$wrapper.find('#r-' + i);
-      init = rows + 1;
-      for (let j = 1; j < rows + 1; j++){
-        let newItem = $(`<div class="cell-wrapper">\
-        <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`);
-        $row.append(newItem);
-      };
-    };
-    this.settings.init = init;
-    this.cacheCells();
-  },
-
-  cacheCells: function(){
-    this.$allCells = this.$wrapper.find('.cell');
-    //console.log(this.$allCells);
-  },
-
-  increaseGridBtn: function(rows, max){
-    this.$increaseBtn.click(function(){
-      if (rows < max){
-        rows ++;
-        let newItem = $(`<div id="r-${rows}" class="_row"></div>`);
-        module.$crossword.append(newItem);
-        for (let i = 1; i < rows; i++){
-          let $row = module.$wrapper.find('#r-' + i);
+        for (let j = 1; j < this.rows + 1; j++){
           let newItem = $(`<div class="cell-wrapper">\
-          <input type="text" maxlength="1" id="${i}.${rows}" class="cell row-${i} col-${rows}" /></div>`);
+          <input type="text" maxlength="1" id="${i}.${j}" class="cell row-${i} col-${j}" /></div>`);
           $row.append(newItem);
         }
-        module.makeCells(rows, module.settings.init);
-      }else{
-        console.log('Exceeds grid size');
+
       }
-    });//end listener
-  },
+      this.cacheCells();
+    },
 
-  selectCells: function(currentWord){
-    this.$crossword.keyup(function(e){
-      console.log(e.target);
-    });
-  },
+    cacheCells: function(){
+      this.$allCells = this.$wrapper.find('.cell');
+    },
 
-  validateGrid: function($allCells){
-    console.log($allCells);
-  }
+    alertBox: function(message){
+      this.$alertMessage.text(message);
+      this.$alertBox.css('display', 'block');
+    },
 
-}//end object
-module.init();
+    alertBoxConfirm: function(){
+      this.$alertBox.css('display', 'none');
+    },
 
+    increaseSize: function(){
+      if (this.rows < this.max){
+        this.rows ++;
+        let newItem = $(`<div id="r-${this.rows}" class="_row"></div>`);
+        this.$crossword.append(newItem);
+        for (let i = 1; i < this.rows; i++){
+          let $row = this.$wrapper.find('#r-' + i);
+          let newItem = $(`<div class="cell-wrapper">\
+          <input type="text" maxlength="1" id="${i}.${this.rows}"\
+           class="cell row-${i} col-${this.rows}" /></div>`);
+          $row.append(newItem);
+        }
+        this.makeCells(this.rows, this.init);
+      }
+      
+      else{
+        const message = 'Exceeds maximum grid size';
+        this.alertBox(message);
+      }
+    },
+
+    runCrosswordEvents: function(e){
+      this.selectCells(e);
+      this.navigateGrid(e);
+    },
+
+    selectCells: function(e){
+      const cell = e.target;
+      const id = cell.id;
+      const a = cell.className.includes('selected');
+      const b = cell.value === '';
+      const c = !this.currentWord.includes(id);
+
+      if (!a && !b && c){
+        cell.classList.add('selected');
+        this.currentWord.push(id);
+      }
+
+      else if (a && b){
+        cell.classList.remove('selected');
+        this.currentWord.pop(id);
+      }
+
+      this.validateGrid(id);
+      this.validateWordLength();
+      this.validateCompleteWord();
+      console.log(this.currentWord);
+    },
+
+    navigateGrid: function(e){
+      if (e.keyCode === 37){
+        const i = this.$allCells.index(e.target);
+        const item = this.$allCells.get(i - 1);
+        item.focus();
+      }
+
+      else if (e.keyCode === 38){
+        const i = this.$allCells.index(e.target);
+        const item = this.$allCells.get(i - this.rows);
+        item.focus();
+      }
+
+      else if (e.keyCode === 39){
+        const i = this.$allCells.index(e.target);
+        const item = this.$allCells.get(i + 1);
+        if (item ? item.focus() : console.log('cannot go further'));
+      }
+
+      else if (e.keyCode === 40){
+        const i = this.$allCells.index(e.target);
+        const item = this.$allCells.get(i + this.rows);
+        if (item ? item.focus() : console.log('cannot go further'));
+      }
+
+    },
+
+    validateGrid: function(id){
+      const sp = id.split(".");
+      const col = sp[0];
+      const row = sp[1];
+      for (let cell of this.$allCells){
+        let $sp = cell.id.split(".");
+        let $col = $sp[0];
+        let $row = $sp[1];
+        if (!(row == $row || col == $col) || cell.className.includes('dead'))
+          cell.disabled = true;
+      }
+    },
+
+    validateWordLength: function(){
+      if (this.currentWord.length < 2)
+        this.$addWordBtn.attr('disabled', true);
+      else
+        this.$addWordBtn.removeAttr('disabled');
+    },
+
+    validateCompleteWord: function(){
+      this.currentWord.sort();
+      let cols = [];
+      let rows = [];
+      let testFails, isRow, isColumn;
+      for (let id of this.currentWord){
+        let sp = id.split(".");
+        cols.push(parseInt(sp[0]));
+        rows.push(parseInt(sp[1]));
+      }
+
+      for (let i=cols.length - 1; i > 0; i--){
+        let j = i - 1;
+        if (cols[i] - cols[j] > 1)
+          testFails = true;
+        else if (rows[i] - rows[j] > 1)
+          testFails = true;
+        else if (cols[i] - cols[j] == 0)
+          isRow = true;
+        else if (rows[i] - rows[j] == 0)
+          isColumn = true;
+      }
+
+      if(testFails)
+        this.$addWordBtn.attr('disabled', true);
+      else if(isRow){
+        this.orientation = 'across';
+      }
+      else if(isColumn){
+        this.orientation = 'down';
+      }
+    }
+
+  }//end object
+  module.init();
+})();
 
 
 
